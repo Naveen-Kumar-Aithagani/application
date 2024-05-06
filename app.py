cfefba9cebcb0af47e6531c0cca5ddfa4ee4d502
NaveenNani07 <165649365+NaveenNani07@users.noreply.github.com>
2024-05-06 14:14:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ a = Label(window ,text = "First Name").grid(row = 0,column = 0)
 b = Label(window ,text = "Last Name").grid(row = 1,column = 0)
 c = Label(window ,text = "Email Id").grid(row = 2,column = 0)
 d = Label(window ,text = "Contact Number").grid(row = 3,column = 0)
-
+#;sfdkhmd'md'pkdgojketpdpdkdghmg';md'hmd'jkma'hasdfjl;kj;lkjf;lasjadslfjkASLJKDF;ASLKDJF;ALJFDLSKDJF;LSDKJF;LASDJF;LASJF;ALSDKJF;LSDKJF;LSs;ldfkja;slkdfj;aslkj
 a1 = Entry(window).grid(row = 0,column = 1)
 b1 = Entry(window).grid(row = 1,column = 1)
 c1 = Entry(window).grid(row = 2,column = 1)
